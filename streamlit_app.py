ebe2581d909650288719eb83ca70109a13337a28
Bhaskar Thalvayapati <115298480+bthalvayapatisnow@users.noreply.github.com>
2022-10-07 14:54:24 -0400

    Update streamlit_app.py
    Added additional streamlit.text statements to the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit;
 
 streamlit.title('I love Labrodar dogs')
+
+streamlit.header('Available dog breeds')
+streamlit.text('We love puppies')
+streamlit.text('Dogs eat lot of food')
+streamlit.text('We have to take them out periodically')
+
