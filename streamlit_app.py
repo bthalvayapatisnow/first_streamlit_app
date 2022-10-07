209b1ff17222fdff795146fa637956b9d040929c
Bhaskar Thalvayapati <115298480+bthalvayapatisnow@users.noreply.github.com>
2022-10-07 15:14:13 -0400

    Update streamlit_app.py
    Added fruits name instead of index

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ streamlit.title('🥣 🥗 🐔 🥑🍞 adding emojis')
 streamlit.header('🥣 🥗 🐔 🥑🍞 adding emojis')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Add user to pick the friuts they want in the smoothies
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
