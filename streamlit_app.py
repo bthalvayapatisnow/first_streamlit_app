1b8382168a96051c2d2158f14f71758f027add54
Bhaskar Thalvayapati <115298480+bthalvayapatisnow@users.noreply.github.com>
2022-10-07 15:10:51 -0400

    Update streamlit_app.py
    Added multiselect picklist and changed the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,9 @@ streamlit.header('🥣 🥗 🐔 🥑🍞 adding emojis')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+#Add user to pick the friuts they want in the smoothies
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#Display the table on the page
+streamlit.dataframe(my_fruit_list)
+
