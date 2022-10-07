ec6bd3ccebd58c42ec5ad3208d5afdccf6a44d0c
Bhaskar Thalvayapati <115298480+bthalvayapatisnow@users.noreply.github.com>
2022-10-07 14:58:58 -0400

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit;
 
 streamlit.title('I love Labrodar dogs')
 
-streamlit.header(':dog Available dog breeds')
+streamlit.header('Available dog breeds')
 streamlit.text('We love puppies')
 streamlit.text('Dogs eat lot of food')
 streamlit.text('We have to take them out periodically')
+
+streamlit.text('🥣 🥗 🐔 🥑🍞 adding emojis')
 
